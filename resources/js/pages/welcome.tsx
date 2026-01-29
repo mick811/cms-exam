77231a7b69cbd041b3f5f03d79d4c8c116d91793
mick811 <mikkelelvers@outlook.com>
2026-01-29 09:59:27 +0100

    init

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,18 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome() {
+    return (
+        <>
+            <Head title="Welcome">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link
+                    href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
+                    rel="stylesheet"
+                />
+            </Head>
+            <div>
+                <h1 className="text-9xl">Hello World</h1>
+            </div>
+        </>
+    );
+}
