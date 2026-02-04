2d63a19de6d4af5f5ba268a95374d3e4fd2bd2c8
mick811 <mikkelelvers@outlook.com>
2026-02-04 21:49:52 +0100

    refactor: update bootstrap imports

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Inertia\Inertia;
-use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
